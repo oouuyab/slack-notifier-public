a41542502e04a0111ebec2eaac4e457539a2023c
oouuyab <oouuyab@gmail.com>
2024-04-23 18:11:42 +0900

    modify: [slack] slack postMessage service 수정

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,19 +1,24 @@
 import {Injectable} from '@nestjs/common';
 import {SlackService} from 'nestjs-slack';
 
+interface PostMessagePayloadInterface {
+  channel: string;
+  blocks: any[];
+}
+
 @Injectable()
 export class AppService {
   constructor(private _slackService: SlackService) {
   }
 
-  async postMessage(param: any): Promise<void> {
+  async postMessage(payload: PostMessagePayloadInterface): Promise<void> {
     try {
       await this._slackService.postMessage({
-        channel: param.channel,
-        blocks: param.block,
+        channel: payload.channel,
+        blocks: payload.blocks
       });
     } catch (error) {
-      console.error(JSON.stringify(error.data));
+      console.error(JSON.stringify(error));
     }
   }
 
